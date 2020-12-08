fc326c53fea17d65dafe9f0a7cdc1781c50620aa
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2020-12-07 21:54:34 -0800

    add platform bouncing

---
+++ Assets/Platform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Rigidbody2D rb = other.collider.GetComponent<Rigidbody2D>();
+        if (rb == null) return;
+
+        Vector2 velocity = rb.velocity;
+        velocity.y = jumpForce;
+        rb.velocity = velocity;
+    }
+}
